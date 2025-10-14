6713d8a6579de1fa7e2921380bd808bfa0630984
Elmir E. Aslanov <elmireas@code.local>
2025-10-14 17:10:13 +0400

    first commit

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,31 @@
+﻿////void Duzbucaqli()
+////{
+////    int a = 4;
+////    int b = 5;
+
+////    int sahe = a * b;
+////    Console.WriteLine(sahe);
+////}
+////Duzbucaqli();
+
+////void DuzbucaqliParalel()
+////{
+////    int a = 8;
+////    int b = 5;
+////    int c = 3;
+
+////    int sahe = 2 * (a * b + a * c + b * c);
+////    Console.WriteLine(sahe);
+////}
+////DuzbucaqliParalel();
+
+//void Ucbucaq()
+//{
+//    int a = 3;
+//    int b = 2;
+//    int c = 5;
+
+//    int p = ( a +b +c ) / 3;
+//    Console.WriteLine(p);
+//}
+//Ucbucaq();
